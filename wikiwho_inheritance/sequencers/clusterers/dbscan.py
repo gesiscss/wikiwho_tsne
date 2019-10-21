61ffddd9c867d404fffa4f4672222d653cbc6c8a
stannida <stannida@gmail.com>
2019-10-21 11:42:49 +0200

    adding silhouette plots

--- wikiwho_inheritance/sequencers/clusterers/dbscan.py
+++ wikiwho_inheritance/sequencers/clusterers/dbscan.py
@@ -9,6 +9,7 @@ import numpy as np
 from sklearn.cluster import DBSCAN
 from sklearn import metrics
 from sequencers.clusterers import clusterer
+from sklearn.metrics import silhouette_samples, silhouette_score
 
 class DBscan(clusterer.Clusterer):
     def transform_feat(self):
@@ -21,6 +22,14 @@ class DBscan(clusterer.Clusterer):
         db = DBSCAN(eps=self.params['eps'], min_samples=self.params['min_samples']).fit(self.transform_feat())
         labels = db.labels_
         self.add_clusters(labels)
+        return labels
+    def silhouette(self):
+        X = self.transform_feat()
+        cluster_labels = self.get_clusters()
+        silhouette_avg = silhouette_score(X, cluster_labels)
+        # Compute the silhouette scores for each sample
+        self.df['silhouette_value'] = silhouette_samples(X, cluster_labels)
+        return silhouette_avg
 
 
 
--- wikiwho_inheritance/sequencers/clusterers/kmeans.py
+++ wikiwho_inheritance/sequencers/clusterers/kmeans.py
@@ -8,6 +8,7 @@ import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
 from sequencers.clusterers import clusterer
+from sklearn.metrics import silhouette_samples, silhouette_score
 
 class Kmeans(clusterer.Clusterer):
     def transform_feat(self):
@@ -20,6 +21,15 @@ class Kmeans(clusterer.Clusterer):
         clusterer = KMeans(random_state=self.params['random_state'])
         clusters = clusterer.fit_predict(np.array(self.transform_feat()))
         self.add_clusters(clusters)
+        return clusters
+    def silhouette(self):
+        X = self.transform_feat()
+        cluster_labels = self.get_clusters()
+        silhouette_avg = silhouette_score(X, cluster_labels)
+        # Compute the silhouette scores for each sample
+        self.df['silhouette_value'] = silhouette_samples(X, cluster_labels)
+        return silhouette_avg
+    
 
 
 
